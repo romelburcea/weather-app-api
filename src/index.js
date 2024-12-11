f35276a6162c57ed3d9f58a38bc663e809a81858
Romel Burcea <romel.burcea@gmail.com>
2024-12-11 21:44:41 +0200

    did the weather location func

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+
+async function getLocation(location){
+    
+    const locationGet = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=X8KY8DYWQDMDVJJ3WU8BZ27UW&contentType=json`)
+    location = await locationGet.json()
+    console.log(location)
+}
+
+getLocation("paris")
