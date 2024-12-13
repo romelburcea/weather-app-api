2c9d73ce6b99ebcd423964419e72927409f57101
Romel Burcea <romel.burcea@gmail.com>
2024-12-13 19:33:16 +0200

    more stuff done

--- src/index.js
+++ src/index.js
@@ -9,12 +9,27 @@ async function getLocation(location){
 }
 
 function getObject(){
-    // const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=X8KY8DYWQDMDVJJ3WU8BZ27UW&contentType=json`
-    getLocation("bucharest") 
-    .then(data => console.log(data.latitude))
-    // const obj = JSON.parse(stuff)
-    // console.log(obj)
+    getLocation("bucharest").then(function(data){
+        const weather = {
+            temperature: data.currentConditions.temp,
+            feelsLike: data.currentConditions.feelsLike,
+            snow: data.currentConditions.snow,
+        }
+
+        console.log(weather)
+        return weather
+    })
+        
+    
 }
+
+getObject()
+
+
+// const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=X8KY8DYWQDMDVJJ3WU8BZ27UW&contentType=json`
+    
+// const obj = JSON.parse(stuff)
+    // console.log(obj)
 
 // // getLocation("london")
 
