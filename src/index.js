1d087b170ddc609246f265d77bee73380b346120
Romel Burcea <romel.burcea@gmail.com>
2024-12-13 15:01:23 +0200

    figured out how to access nested stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import "./styles.css";
 
 async function getLocation(location){
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=X8KY8DYWQDMDVJJ3WU8BZ27UW&contentType=json`
+    const response = await fetch(url)
+    return response.json()
+  
     
-    const locationGet = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=X8KY8DYWQDMDVJJ3WU8BZ27UW&contentType=json`)
-    location = await locationGet.json()
-    console.log(location)
 }
 
-getLocation("paris")
+function getObject(){
+    // const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=X8KY8DYWQDMDVJJ3WU8BZ27UW&contentType=json`
+    getLocation("bucharest") 
+    .then(data => console.log(data.latitude))
+    // const obj = JSON.parse(stuff)
+    // console.log(obj)
+}
+
+// // getLocation("london")
+
+// getObject()
+// getLocation("bucharest")
+
+// fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/bucharest?unitGroup=metric&key=X8KY8DYWQDMDVJJ3WU8BZ27UW&contentType=json")
+//     .then(response => response.json())
+//     .then(data => console.log(data.currentConditions.temp))
